b127ead36e24bc106f781e135db2bbcd436d7587
fochoa <fochoa@ucsd.edu>
2018-02-08 11:44:59 -0800

    lab5 work

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -44,3 +44,4 @@ function anagrammedName(name) {
 		return name;
 	}
 }
+
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@
 var data = require('../data.json');
 
 exports.view = function(req, res){
+	res.render('index',data);
 	console.log(data);
-	res.render('index');
 };
